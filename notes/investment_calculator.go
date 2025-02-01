3b2e8c1d23a76edeb3dc1c6447bdbaccc12c886e
DonovanOLinn <donovan.b.olinn@gmail.com>
2025-02-01 15:18:48 -0600

    function notes

--- notes/investment_calculator.go
+++ notes/investment_calculator.go
@@ -5,21 +5,42 @@ import (
 	"math"
 )
 
+const inflationRate float64 = 6.5
+
+
 func main() {
-	const inflationRate float64 = 6.5
+	
 	var investmentAmount float64
-	expectedReturnRate := 5.5
+	var expectedReturnRate = 5.5
 	var years float64 = 10
 
-	fmt.Print("Investment Amount: ")
+	// fmt.Print("Investment Amount: ")
+	outputText("Investment Amount: ")
 	fmt.Scan(&investmentAmount)
-	fmt.Print("Expected Return Rate: ")
+	// fmt.Print("Expected Return Rate: ")
+	outputText("Expected return Rate: ")
 	fmt.Scan(&expectedReturnRate)
 	fmt.Print("Years Invested: ")
 	fmt.Scan(&years)
 
-	futureValue := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
-	futureRealValue := futureValue / math.Pow((1+inflationRate/100), years)
-	fmt.Println(futureValue)
-	fmt.Println(futureRealValue)
+	futureValue, futureRealValue := calculateFutureValue(investmentAmount, expectedReturnRate, years)
+
+	// futureValue := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
+	// futureRealValue := futureValue / math.Pow((1+inflationRate/100), years)
+	// fmt.Println("Future Value:", futureValue)
+	fmt.Printf("Future Value: %.2f\nFuture Value (adjusted for Inflation): %.2f", futureValue, futureRealValue)
+	fmt.Println("Future Value (adjusted for Inflation): ", futureRealValue)
+}
+
+func outputText(text string) {
+	fmt.Print(text)
+}
+
+func calculateFutureValue(investmentAmount float64, expectedReturnRate float64, years float64) (fv float64, rfv float64) {
+	// fv := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
+	// rfv := fv / math.Pow((1+inflationRate/100), years)
+	// IF YOU DECLARE YOUR RETURN VALUES IN THE RETURN DECLARATION ABOVE, YOU DON'T NEED TO DEFINE THEM IN CODE
+	fv = investmentAmount * math.Pow(1+expectedReturnRate/100, years)
+	rfv = fv / math.Pow((1+inflationRate/100), years)
+	return fv, rfv 
 }
