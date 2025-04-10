65daf40e1aa043f2a28eaee73f4d78b7dbae5595
DonovanOLinn <donovan.b.olinn@gmail.com>
2025-04-10 07:35:55 -0500

    Adding dummy json data, cmdmanager, conversio, filemanager, and iomanager packages, along with updates to price-calculator main.go file

--- price-calculator/main.go
+++ price-calculator/main.go
@@ -1,18 +1,28 @@
 package main
 
 import (
-	"example.com/price-calc/prices"
 	"fmt"
+
+	"example.com/price-calc/cmdmanager"
+	// "example.com/price-calc/filemanager"
+	"example.com/price-calc/prices"
 )
+
+
 
 func main() {
 	var taxRates []float64 = []float64{0,0.07,0.10,0.15}
 
-	// var result map[float64][]float64 = make(map[float64][]float64)
-
 	for _, taxRate := range taxRates {
-		priceJob := prices.NewTaxIncludedPriceJob(taxRate)
-		priceJob.Process()
+		// fm := filemanager.New("prices.txt", fmt.Sprintf("result_%.0f.json", taxRate*100))
+		cmdm := cmdmanager.New()
+		priceJob := prices.NewTaxIncludedPriceJob(cmdm, taxRate)
+		err := priceJob.Process()
+
+		if err != nil {
+			fmt.Println("Could not process job")
+			fmt.Println(err)
+		}
 	}
 
 }
